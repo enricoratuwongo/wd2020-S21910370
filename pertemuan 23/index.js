9414cb39baeb2f20dee8e947fc10fa285f641a1f
enricoratuwongo <enricowtf@gmail.com>
2020-11-09 23:25:46 +0800

    excercise #7 in pertemuan 23

---
+++ pertemuan 23/index.js
@@ -0,0 +1,66 @@
+//console.log("Hello...");
+
+//let & const
+//let umur = 18;
+//umur = 19;
+//let nilai = 95.9;
+//let sudahmenikah = false;
+//let nama = koko;
+//console.log(umur);
+//console.log(nama);
+
+//let alamat;
+//console.log(alamat);
+
+//const tahunlahir = 2001
+//console.log(tahunlahir);
+
+//operator
+//arithmetic Operator (+,-,*,/)
+
+//let angka1 = 20;
+//let angka2 = 10;
+//console.log(angka1 + angka2);
+
+//let namaDepan = "Enrico";
+//let namaBelakang = "Ratuwongo";
+//let umur = 30;
+//console.log(
+    //"nama Saya " +
+    //namaDepan + " " +
+    //namaBelakang +
+    //". Umur saya adalah " +
+    //umur +
+    //"tahun"
+//);
+
+//operator perbandingan
+//let angka3 = 10;
+//let angka4 = 10;
+
+//console.log(angka3 == angka4);
+
+//ternary operator
+//let angka= 5;
+//let hasil = angka <= 0 ? "Negatif" : "Positif";
+//console.log(hasil);
+
+//operator logika
+//let kondisi1 = true;
+//let kondisi2 = false;
+//console.log(kondisi1 && kondisi2);
+//scanf
+//let angka = prompt("masukan angka :");
+//console.log("angka yang dimasukan adalah" + angka);
+// exercise #7
+// buatlah program java script untuk menghitung BMI
+// rumus = berat (kg) / (tinggi * tinggi)
+// gunakan prompt 
+
+//jawaban excercize #7
+    let beratbadan = prompt("masukan berat badan :");
+    let tinggibadan = prompt("masukan tinggi :");
+        let bmi = beratbadan/(tinggibadan/100*tinggibadan/100);
+        console.log("BMI anda adalah :" + bmi);
+    
+        
